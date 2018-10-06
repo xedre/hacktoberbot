2c39907a8cbea822a53afef9de1b16b71f0d2538
xedre <git@xed.re>
2018-10-06 10:08:23 +0100

    added logging and made new bot class

--- __init__.py
+++ __init__.py
@@ -1,1 +1,23 @@
 import logging
+import os
+import sys
+import bot.bot
+from logging import handlers
+from discord.ext import commands
+
+logging_handlers = [logging.StreamHandler(stream=sys.stderr),
+                    handlers.TimedRotatingFileHandler("logs/log.txt", when="s", interval=5, utc=True)]
+
+logging.basicConfig(
+    format="%(asctime)s Bot: | %(name)30s | %(levelname)8s | %(message)s",
+    datefmt="%b %d %H:%M:%S",
+    handlers=logging_handlers
+)
+
+
+hackbot = bot.bot.Hacktoberbot(command_prefix=commands.when_mentioned_or('!'))
+hackbot.load_cogs()
+hackbot.startup()
+
+logging.info("Closing down")
+logging.shutdown()
--- bot/bot.py
+++ bot/bot.py
@@ -2,21 +2,36 @@ from pathlib import Path
 from sys import stderr
 from traceback import print_exc
 from os import environ
+import logging
 
 from discord.ext import commands
 
 
-HACKTOBERBOT_TOKEN = environ.get('HACKTOBERBOT_TOKEN')
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'))
+class Hacktoberbot(commands.Bot):
+    HACKTOBERBOT_TOKEN = environ.get('HACKTOBERBOT_TOKEN')
+
+    def load_cogs(self):
+        # Scan for files in the /cogs/ directory and make a list of the file names.
+        cogs = [file.stem for file in Path('cogs').glob('*.py')]
+        for extension in cogs:
+            try:
+                bot.load_extension(f'cogs.{extension}')
+            except Exception as e:
+                logging.critical(f'Failed to load extension {extension}.')
+                print_exc()
+
+    def startup(self):
+        logging.info("Connecting")
+        self.run(self.HACKTOBERBOT_TOKEN)
+        logging.info("Started up")
+
+    def __init__(self, command_prefix, formatter=None, description=None, pm_help=False, **options):
+        logging.info("Initialising")
+        super(Hacktoberbot, self).__init__(command_prefix=command_prefix, formatter=formatter,
+                                           description=description, pm_help=pm_help, options=options)
+
 
 if __name__ == '__main__':
-    # Scan for files in the /cogs/ directory and make a list of the file names.
-    cogs = [file.stem for file in Path('cogs').glob('*.py')]
-    for extension in cogs:
-        try:
-            bot.load_extension(f'cogs.{extension}')
-        except Exception as e:
-            print(f'Failed to load extension {extension}.', file=stderr)
-            print_exc()
-
-bot.run(HACKTOBERBOT_TOKEN)
+    bot = Hacktoberbot(command_prefix=commands.when_mentioned_or('!'))
+    bot.load_cogs()
+    bot.startup()
